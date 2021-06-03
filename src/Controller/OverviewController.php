ba7fb6b48b33631449b63e866b69237833212b01
dboling19 <68087576+dboling19@users.noreply.github.com>
2021-06-03 18:00:40 -0400

    Changes according to David's comments on 2nd commit

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -6,10 +6,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpKernel\Kernel;
 use App\Entity\Articles;
 
 class OverviewController extends AbstractController
 {
+
+  /**
+   * Simply adds a row into the database through a standard function
+   * Will eventually have an input form for users to input things like
+   * subject and text, their name and email will probably be pulled
+   * automatically through the authenticator (assuming I have access to
+   * that in this file)
+   * 
+   * @return string and id of new test article
+   * 
+   */
 
   /**
    * @Route("/add", name="add_info")
@@ -17,9 +29,9 @@ class OverviewController extends AbstractController
   public function add_info(): Response
   {
 
-    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager = $this->getDoctrine()->getManager(); // SiAAAmply understanding this as a basic "rule" of symfony;
 
-    $article = new Articles();
+    $article = new Articles();    // Init the articles object for the Articles table. Calls found in /src/Entity/Articles.php;
     $articleId = strval($article->getId());
     $article->setSubject('testSubject'.$articleId);
     $article->setAuthor('testAuthor'.$articleId);
@@ -33,6 +45,15 @@ class OverviewController extends AbstractController
   }
 
   /**
+   * This should be the main page that everyone should see. Ever user (not sure
+   * if we're doing guest users or not) should be able to see this page and everything
+   * on it. This will be modified more clearly from it's current state. Currently
+   * being used as a testing stage for database outputs.
+   * 
+   * @return rendered overview.html.twig
+   */
+
+  /**
   * @Route("/overview", name="show_all")
   */
   public function show_all(): Response
@@ -41,11 +62,11 @@ class OverviewController extends AbstractController
     $date = getdate();
 
     $articles = $this->getDoctrine()
-      ->getRepository(Articles::class)
-      ->findAll();
+      ->getRepository(Articles::class)    // inits the database and table Articles;
+      ->findAll();    // defined in /src/Entity/Articles.php;
     
-      if(!$articles){
-        throw $this->createNotFoundException(
+      if($articles == null){   // testing for blank database or values;
+        throw $this->createNotFoundException(   // I don't like this resolution very much, but it works for now;
           'No articles found!'
         );
       }
