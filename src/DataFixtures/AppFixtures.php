e614e43a8138e749969d05a94c97bb6fb43528ed
davidwkdavidwk <davidwk@goshen.edu>
2021-06-21 15:51:28 -0400

    Added articles to the AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $article = new Articles();    
+        $article->setSubject('testSubject');
+        $article->setAuthor('testAuthor');
+        $article->setText('testText');
+        $manager->persist($article);
 
         $manager->flush();
     }
