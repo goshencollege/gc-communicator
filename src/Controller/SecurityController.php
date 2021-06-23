270bb05a55f30d492eff2563b2c0308e217bac01
dboling19 <68087576+dboling19@users.noreply.github.com>
2021-06-23 12:16:32 -0400

    Formatting fixes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,4 +33,7 @@ class SecurityController extends AbstractController
   {
     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
   }
+
 }
+
+// EOF
