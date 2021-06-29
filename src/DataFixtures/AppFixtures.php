2e7b596744191f771905542f9709ef12665157fd
davidwkdavidwk <davidwk@goshen.edu>
2021-06-29 07:08:51 -0400

    Fixed AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Article;
+use App\Entity\Articles;
 
 class AppFixtures extends Fixture
 {
