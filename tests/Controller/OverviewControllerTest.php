f578848829faa4181165e05f27a242826d0400a3
Daniel Boling <dbdanielbboling70@gmail.com>
2021-09-14 09:27:54 -0400

    Initial Changes commit

--- tests/Controller/OverviewControllerTest.php
+++ tests/Controller/OverviewControllerTest.php
@@ -3,6 +3,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class OverviewControllerTest extends WebTestCase
 {
@@ -10,11 +11,12 @@ class OverviewControllerTest extends WebTestCase
     /**
      * Load the /add URL and ensure that it gets a 200 status code
      * 
-     * @author David King
+     * @author Daniel Boling
      * 
      */
-    public function testAdd()
+    public function test_add()
     {
+
         $client = static::createClient();
 
         // This should return 302 since no user is authenticated
@@ -23,8 +25,8 @@ class OverviewControllerTest extends WebTestCase
         $this->assertEquals(302, $response->getStatusCode());
 
         // authenticate a test user
-        $userRepo = static::$container->get(UserRepository::class);
-        $testUser = $userRepo->findOneByUsername("david");
+        $userRepo = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepo->findOneByUsername("test_user");
         $client->loginUser($testUser);
 
         // This should now return 200 since a user is authenticated
@@ -39,13 +41,14 @@ class OverviewControllerTest extends WebTestCase
      * @author David King
      * 
      */
-    public function testOverview()
+    public function test_overview()
     {
+
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/overview');
-
+        $client->request('GET', '/overview');
         $response = $client->getResponse();
+
         $this->assertEquals(200, $response->getStatusCode());
 
     }
