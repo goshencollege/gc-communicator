b36de7eb9e3aa042926e9556ac3f2e1ca3cdd363
Daniel Boling <dbdanielbboling70@gmail.com>
2021-11-17 15:09:23 -0500

    Controller test complete

--- tests/Controller/OverviewControllerTest.php
+++ tests/Controller/OverviewControllerTest.php
@@ -100,13 +100,71 @@ class OverviewControllerTest extends WebTestCase
         $form = $buttonCrawlerNode->form();
         $pre_form_fields = $form->getValues();
 
+        $announcement_subject = $pre_form_fields['announcement_form[subject]'];
+        $announcement_author = $pre_form_fields['announcement_form[author]'];
+        $this->assertSame($announcement->getSubject(), $announcement_subject);
+        $this->assertSame($announcement->getAuthor(), $announcement_author);
+
         $client->submit($form, [
             'announcement_form[subject]' => 'Change',
             'announcement_form[text]' => 'change lorem ispum',
         ]);
         
+        $post_form_fields = $form->getValues();
+
+        // returns last result of all announcements
+        $announcement = static::getContainer()
+            ->get(AnnouncementRepository::class)
+            ->findAll()
+        ;
+        $announcement = end($announcement);
+
+        $announcement_subject = $post_form_fields['announcement_form[subject]'];
+        $announcement_author = $post_form_fields['announcement_form[author]'];
+        $this->assertSame($announcement->getSubject(), $announcement_subject);
+        $this->assertSame($announcement->getAuthor(), $announcement_author);
 
 
+        // authenticate a moderator user and check that the page returns a 200 code
+        $moderator_user = static::getContainer()
+            ->get(UserRepository::class)
+            ->findOneByUsername('dboling')
+        ;
+        $client->loginUser($moderator_user);
+        $client->request('GET', '/modify/announcement/' . $announcement->getId());
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $crawler = $client->request('GET', '/modify/announcement/' . $announcement->getId());
+        $buttonCrawlerNode = $crawler->selectButton('Submit Announcement');
+        $form = $buttonCrawlerNode->form();
+        $pre_form_fields = $form->getValues();
+
+        $announcement_subject = $pre_form_fields['announcement_form[subject]'];
+        $announcement_author = $pre_form_fields['announcement_form[author]'];
+        $this->assertSame($announcement->getSubject(), $announcement_subject);
+        $this->assertSame($announcement->getAuthor(), $announcement_author);
+
+        $client->submit($form, [
+            'announcement_form[subject]' => 'Change',
+            'announcement_form[text]' => 'change lorem ispum',
+        ]);
+        
+        $post_form_fields = $form->getValues();
+
+        // returns last result of all announcements
+        $announcement = static::getContainer()
+            ->get(AnnouncementRepository::class)
+            ->findAll()
+        ;
+        $announcement = end($announcement);
+        
+        $announcement_subject = $post_form_fields['announcement_form[subject]'];
+        $announcement_author = $post_form_fields['announcement_form[author]'];
+        $this->assertSame($announcement->getSubject(), $announcement_subject);
+        $this->assertSame($announcement->getAuthor(), $announcement_author);
+
+        
     }
 }
 
