2ac737d5d6186f430b4b3c68a560af5db4d46113
Daniel Boling <dbdanielbboling70@gmail.com>
2022-03-14 15:35:08 -0400

    Typos and functionality finalized

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -54,7 +54,7 @@ class AppFixtures extends Fixture
 
 
     $test_user = new User();
-    $test_user->setUsername('johndoe');
+    $test_user->setUsername('fixtures_user');
     $test_user->setRoles(['ROLE_USER']);
     $test_user->setPassword($this->passwordHasher->hashPassword(
         $test_user,
@@ -70,13 +70,13 @@ class AppFixtures extends Fixture
     // one announcement set to a past date, one to the current date (constant) and one for a future date
     $announcement_date = new \DateTime('now', new \DateTimeZone('GMT'));
     $announcement = new Announcement();
-    $announcement->setSubject('Fixture Subject 1');
-    $announcement->setAuthor('John Doe');
+    $announcement->setSubject('Make your own beeswax wraps!');
+    $announcement->setAuthor('Amy Wahl');
     $announcement->setCategory($test_cat);
     $announcement->setUser($test_user);
     $announcement->setStartDate($announcement_date);
     $announcement->setEndDate($announcement_date);
-    $announcement->setText('Lorem Ipsum');
+    $announcement->setText('Join Merry Lea for a hands-on workshop to create your very own beeswax cloth wrap. This handy addition to the kitchen provides an alternative to plastic wrap or single-use plastic bags. Select a cloth pattern of your choice and learn how to prepare the ingredients to make this sustainable tool. Participants will go home with one large and one small beeswax wrap.');
     $announcement->setApproval(1);
     $manager->persist($announcement);
 
@@ -88,13 +88,19 @@ class AppFixtures extends Fixture
     // one announcement set to a past date, one to the current date (constant) and one for a future date
     $announcement_date = new \DateTime('now', new \DateTimeZone('GMT'));
     $announcement = new Announcement();
-    $announcement->setSubject('Fixture Subject 2');
-    $announcement->setAuthor('John Doe');
+    $announcement->setSubject('Transfer, Adult & Graduate Open House');
+    $announcement->setAuthor('Paul Ritcher');
     $announcement->setCategory($test_cat);
     $announcement->setUser($test_user);
     $announcement->setStartDate($announcement_date);
     $announcement->setEndDate($announcement_date);
-    $announcement->setText('Lorem Ipsum');
+    $announcement->setText('Transfer, Adult & Graduate Open House
+
+    Join us for an open house tailored for individuals interested in learning more about:
+    
+    *Transferring into Goshen College’s traditional undergraduate program;
+    *Our degree-completion and licensure programs catered toward working adults; or
+    *Our top-notch graduate programs.');
     $announcement->setApproval(1);
     $manager->persist($announcement);
 
@@ -106,13 +112,13 @@ class AppFixtures extends Fixture
     // one announcement set to a past date, one to the current date (constant) and one for a future date
     $announcement_date = new \DateTime('now', new \DateTimeZone('GMT'));
     $announcement = new Announcement();
-    $announcement->setSubject('Fixture Subject 3');
-    $announcement->setAuthor('John Doe');
+    $announcement->setSubject('St. Patrick\'s Day Potluck Celebration, Thurs., March 17th.');
+    $announcement->setAuthor('Daniel Boling');
     $announcement->setCategory($test_cat);
     $announcement->setUser($test_user);
     $announcement->setStartDate($announcement_date);
     $announcement->setEndDate($announcement_date);
-    $announcement->setText('Lorem Ipsum');
+    $announcement->setText('The Commuter Student Association invites you to come to our potluck lunch St. Patrick\'s Day Celebration on March 17th. from 11 am - 2 pm at the Leadership Hub (Union Building, next to Leaf Raker)');
     $announcement->setApproval(1);
     $manager->persist($announcement);
 
@@ -125,13 +131,13 @@ class AppFixtures extends Fixture
     // one announcement set to a past date, one to the current date (constant) and one for a future date
     $announcement_date = new \DateTime('now', new \DateTimeZone('GMT'));
     $announcement = new Announcement();
-    $announcement->setSubject('Fixture Subject 4');
-    $announcement->setAuthor('John Doe');
+    $announcement->setSubject('This Week\'s Convo: Community Engagement Fair');
+    $announcement->setAuthor('David Blanch');
     $announcement->setCategory($test_cat);
     $announcement->setUser($test_user);
     $announcement->setStartDate($announcement_date);
     $announcement->setEndDate($announcement_date);
-    $announcement->setText('Lorem Ipsum');
+    $announcement->setText('This week for convo, we\'ll hear from some local Goshen residents, Shannan and Cory Martin, about how they invest in the local community. From 10:30 to 12:00, local agencies will be set up in the fellowship hall where you\'ll have a chance to connect around volunteer, internship, and work opportunities. Drinks, snacks, and door prizes from local organizations are part of the mix.');
     $announcement->setApproval(1);
     $manager->persist($announcement);
 
